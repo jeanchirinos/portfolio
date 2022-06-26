9ec660a471378cf72c1070e08882e8348607ccad
Jean Chirinos <jeanchirinos17@gmail.com>
2022-06-25 20:03:58 -0500

    last change ?

--- components/Atoms/SmallIsotype.jsx
+++ components/Atoms/SmallIsotype.jsx
@@ -1,21 +1,28 @@
 import styled from 'styled-components';
 import useTechnology from 'components/Atoms/AllTechnologies';
-import { S_Isotype } from 'components/StyledComponents';
 
 export default function SmallIsotype({ name }) {
-  const { component, color } = useTechnology(name);
+  const { component, color, fullName } = useTechnology(name);
 
   const props = {
     color,
     viewBox: '0 0 104 104',
   };
 
-  return <S_SmallIsotype {...props}>{component}</S_SmallIsotype>;
+  const title = fullName || name;
+
+  return (
+    <S_SmallIsotype {...props}>
+      <>
+        <title>{title}</title>
+        {component}
+      </>
+    </S_SmallIsotype>
+  );
 }
 
 const S_SmallIsotype = styled.svg`
   width: 1.3rem;
   height: 1.3rem;
   fill: transparent;
-  pointer-events: none;
 `;
